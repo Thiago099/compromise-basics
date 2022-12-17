9fdc30d74d3dc643a00d4b9ce87eac937ebfde22
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2022-12-17 10:45:38 -0300

    [feature]

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const nlp = require('compromise');
 
 function extractMeaning(combination) {
-  const result = nlp(combination).out("json").map(x=>x.terms.map(x=>{return {text:x.text,tags:x.tags}}))
+  const result = nlp(combination)
+      .out("json")
+      .map(x => x.terms.map(x => {return {name:x.text,tags:x.tags}}))
   return result;
 }
 
-console.log(extractMeaning("The cat is sleeping."));
+console.log(JSON.stringify(extractMeaning("The cat is sleeping."),null,4));
